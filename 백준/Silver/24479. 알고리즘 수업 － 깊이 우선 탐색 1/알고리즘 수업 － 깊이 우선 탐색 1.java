ce028f46a916264ef283cb33c2bfd8f38345c659
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2023-12-04 19:14:59 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1060 ms, Memory: 87652 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N, M, R, cnt;
+	static ArrayList<ArrayList<Integer>> list;
+	static int[] check;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken()); //vertex
+		M = Integer.parseInt(st.nextToken()); //edge
+		R = Integer.parseInt(st.nextToken()); //startVer
+		
+		list = new ArrayList<>();
+		for(int i = 0; i <= N; i++) {
+			list.add(new ArrayList<>());
+		}
+		
+		check = new int[N + 1];
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			list.get(a).add(b);
+			list.get(b).add(a);
+		}
+		
+		for(int i = 1; i < list.size(); i++) {
+			Collections.sort(list.get(i));
+		}
+		
+		cnt = 1;
+		
+		dfs(R);
+		
+		for(int i = 1; i < check.length; i++) {
+			sb.append(check[i]).append("\n");
+		}
+		
+		System.out.println(sb);
+
+	}
+	
+	public static void dfs(int vertex) {
+		check[vertex] = cnt;
+		
+		for(int i = 0; i < list.get(vertex).size(); i++) {
+			int newVertex = list.get(vertex).get(i);
+			
+			if(check[newVertex] == 0) {
+				cnt++;
+				dfs(newVertex);
+			}
+		}
+	}
+
+}
