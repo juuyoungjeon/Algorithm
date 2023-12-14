3493d63a831f393729713efec17592dba393eb72
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2023-12-14 10:31:00 +0900

    [Gold V] Title: 입국심사, Time: 504 ms, Memory: 28404 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -1,53 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
+
 public class Main {
-    static int n;
-    static long m, max;
-    static int [] arr;
-    static long result = Long.MAX_VALUE;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-        String[] s = br.readLine().split(" ");
-        n = Integer.parseInt(s[0]);
-        m = Integer.parseInt(s[1]);
- 
-        arr = new int[n];
-        for(int i=0; i<n; i++){
-            arr[i] = Integer.parseInt(br.readLine());
-            max = Math.max(max,arr[i]);
-        }
-        Arrays.sort(arr);
- 
-        solve();
- 
-        System.out.println(result);
-    }
- 
-    private static void solve(){
-        long low = 0;
-        long high = m * max;
- 
-        while(low<=high){
-            long mid = (low+high)/2;
-            long sum = 0;
-            for(long index: arr){
-                long count = mid/index;
- 
-                if(sum>=m){
-                    break;
-                }
-                sum+=count;
-            }
-            if(sum>=m){
-                high = mid-1;
-                result = Math.min(mid,result);
-            }
-            else{
-                low = mid+1;
-            }
-        }
-    }
+	static long N,M,max;
+	static long[] arr;
+	static long result = Long.MAX_VALUE;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Long.parseLong(st.nextToken());
+		M = Long.parseLong(st.nextToken());
+		
+		arr = new long[(int)N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Long.parseLong(br.readLine());
+			max = Math.max(max, arr[i]);
+		}
+		
+		Arrays.sort(arr);
+		
+		solve();
+		
+		System.out.println(result);
+		
+	}
+	public static void solve() {
+		long low = 0;
+		long high = M*max;
+		
+		while(low<=high) {
+			long mid = (low+high)/2;
+			long sum = 0;
+			
+			for(long idx : arr) {
+				long cnt = mid/idx;
+				
+				if(sum >= M) {
+					break;
+				}
+				sum+=cnt;
+			}
+			if(sum >= M) {
+				high = mid - 1;
+				result = Math.min(mid, result);
+			}
+			else {
+				low = mid + 1;
+			}
+		}
+	}
+
 }
