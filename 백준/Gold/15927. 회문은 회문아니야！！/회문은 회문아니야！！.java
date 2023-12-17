9d2f0a4b692962a3e291bfd5b4e4700fc40bdf72
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2023-12-17 19:03:54 +0900

    [Gold V] Title: 회문은 회문아니야!!, Time: 232 ms, Memory: 19024 KB -BaekjoonHub

---
+++ 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s= br.readLine();
+		
+		boolean f = false;
+		int len = s.length();
+		for(int i=0; i<len/2; i++) {
+			if(s.charAt(i) != s.charAt(len-i-1)) {
+				System.out.println(s.length());
+				return;
+			}else if(s.charAt(i) != s.charAt(i+1)) {
+				f = true;
+			}
+			
+		}
+		if(f) {
+			System.out.println(s.length()-1);
+		}else {
+			System.out.println(-1);
+		}
+	}
+}
