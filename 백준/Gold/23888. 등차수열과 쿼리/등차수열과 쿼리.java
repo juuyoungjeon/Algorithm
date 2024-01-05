0a3b9415d022cf618ddef200bb2a09f585d7abd6
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-01-05 14:32:26 +0900

    [Gold V] Title: 등차수열과 쿼리, Time: 1160 ms, Memory: 307092 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/23888. 등차수열과 쿼리/등차수열과 쿼리.java
+++ 백준/Gold/23888. 등차수열과 쿼리/등차수열과 쿼리.java
@@ -1,51 +1,54 @@
-import java.util.*;
 import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import static java.lang.System.*;
 
 public class Main {
-	static long a, d;
-	static long q;
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		a = Long.parseLong(st.nextToken());
-		d = Long.parseLong(st.nextToken());
-		
-		q = Long.parseLong(br.readLine());
-		
-		for(long i = 0; i < q; i++) {
-			st = new StringTokenizer(br.readLine());
-			int type = Integer.parseInt(st.nextToken());
-			long l = Long.parseLong(st.nextToken());
-			long r = Long.parseLong(st.nextToken());
-			
-			if(type == 1) {
-				long Al = a + (l-1)*d;
-				long An = a + (r-1)*d;
-				long sum = ((Al+An)*(r-l+1)/2);
-				sb.append(sum).append("\n");
-			}else {
-				long Al = a + (l-1)*d;
-				long ans = 0;
-				if(l==r) {
-					sb.append(Al).append("\n");
-				}
-				else {					
-					for(long j = 0; j < r-l+1; j++) {
-						long b = Al + d;
-						ans = GCD(Al, b);
-					}
-					sb.append(ans).append("\n");
-				}
-			}
-		}
-		System.out.println(sb);
-	}
-	
-	public static long GCD(long A, long B) {
-		if(B==0) return A;
+    static long a;
+    static long d;
+    static long ans_2;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+        a = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+        long a1 = Math.max(a, d);
+        long d1 = Math.min(a, d);
+        ans_2 = gcd(a1, d1);//2번쿼리 답이 연속된 두항이상이면 무조건 답이 이거.
+        String str1 = br.readLine();
+        StringTokenizer st1 = new StringTokenizer(str1);
+        int T = Integer.parseInt(st1.nextToken());
+        for (int i = 0; i < T; i++) {
+            String str2 = br.readLine();
+            StringTokenizer st2 = new StringTokenizer(str2);
+            int type = Integer.parseInt(st2.nextToken());
+            long l = Integer.parseInt(st2.nextToken());
+            long r = Integer.parseInt(st2.nextToken());
+            if(type==1) {
+                String s = String.valueOf(((a+(l-1)*d)+(a+(r-1)*d))*(r-l+1)/2);
+                bw.write(s);
+                bw.newLine();
+            }
+            else {
+                if(l==r) {
+                    String s = String.valueOf((a+(l-1)*d));
+                    bw.write(s);
+                    bw.newLine();
+                }
+                else {
+                    String s = String.valueOf(ans_2);
+                    bw.write(s);
+                    bw.newLine();
+                }
+            }
+        }
+        bw.close();
+    }
+    public static long gcd(long A, long B){//A가 큰거, B가 작은거.
+        if(B==0) return A;
         while (true){
             if(A%B==0) break;
             long tmp = B;
@@ -53,6 +56,6 @@ public class Main {
             A=tmp;
         }
         return B;
-	}
+    }
 
 }
