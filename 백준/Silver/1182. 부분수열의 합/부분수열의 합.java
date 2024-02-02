5eb6d641867cbdd9a92a6bcd8dd9c26636eaeba9
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-02-02 22:17:11 +0900

    [Silver II] Title: 부분수열의 합, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N, S;
+	static int[] arr, result;
+	static int cnt;
+	static boolean[] visited;
+	static ArrayList<Integer> list;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+
+		arr = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		dfs(0, 0);
+		
+		System.out.println(S == 0? cnt-1:cnt);
+	}
+
+	public static void dfs(int depth, int sum) {
+			if(depth == N) {
+				if(sum == S) {
+					cnt++;
+				}
+				return;
+			}
+			dfs(depth+1, sum+arr[depth]);
+			dfs(depth+1,sum);
+	}
+}
