4dbdbab1b30576ef6d54d7b70fd5558890458194
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-03-11 19:40:49 +0900

    [Gold V] Title: 숫자고르기, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -2,42 +2,46 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N;
-	static ArrayList<Integer> list;
-	static int[] arr;
+	static int n;
 	static boolean[] visited;
-	
+	static int[] arr;
+	static ArrayList<Integer> list;
 	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		n = Integer.parseInt(br.readLine());
 		
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N + 1];
-		for(int i = 1; i <= N; i++) {
+		arr = new int[n+1];
+		for(int i = 1; i <= n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 		
 		list = new ArrayList<>();
-		visited = new boolean[N + 1];
-		for(int i = 1; i <= N; i++) {
+		visited = new boolean[n+1];
+		
+		for(int i = 1; i <= n; i++) {
 			visited[i] = true;
-			dfs(i, i);
+			dfs(i,i);
 			visited[i] = false;
 		}
 		
 		Collections.sort(list);
-		System.out.println(list.size());
-		for(int i = 0; i < list.size(); i++) {
-			System.out.println(list.get(i));
+		sb.append(list.size()).append("\n");
+		for(int i : list) {
+			sb.append(i).append("\n");
 		}
+		System.out.println(sb);
+		
 	}
-
-	private static void dfs(int start, int target) {
-		if(visited[arr[start]] == false) {
+	public static void dfs(int start, int target) {
+		if(!visited[arr[start]]) {
 			visited[arr[start]] = true;
 			dfs(arr[start], target);
 			visited[arr[start]] = false;
 		}
-		if(arr[start] == target) list.add(target);
+		if(arr[start] == target) {
+			list.add(target);
+		}
 	}
-
 }
