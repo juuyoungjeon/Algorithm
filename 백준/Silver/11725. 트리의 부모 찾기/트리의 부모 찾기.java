350755684b4509f0c1f9152fb17a1cd11043fbb1
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-03-28 15:15:08 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1356 ms, Memory: 83536 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -2,52 +2,48 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	static ArrayList<ArrayList<Integer>> list;
 	static int N;
-	static ArrayList<ArrayList<Integer>> tree;
-	static int[] parents;
 	static boolean[] visited;
-	
+	static int[] parents;
 	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
 		N = Integer.parseInt(br.readLine());
 		
-		tree = new ArrayList<>();
-		for(int i = 0; i < N + 1; i++) {
-			tree.add(new ArrayList<>());
+		list = new ArrayList<>();
+		for(int i = 0; i <= N; i++) {
+			list.add(new ArrayList<>());
 		}
 		
-		for(int i = 1; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N - 1; i++) {
+			st = new StringTokenizer(br.readLine());
+			
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			tree.get(a).add(b);
-			tree.get(b).add(a);
+			
+			list.get(a).add(b);
+			list.get(b).add(a);
 		}
 		
-		parents = new int[N + 1];
 		visited = new boolean[N + 1];
-		bfs();
+		parents = new int[N + 1];
+		
+		dfs(1);
 		for(int i = 2; i <= N; i++) {
 			System.out.println(parents[i]);
 		}
-		
 	}
-
-	private static void bfs() {
-		Queue<Integer> q = new LinkedList<>();
-		q.add(1);
-		visited[1] = true;
+	
+	public static void dfs(int index) {
+		visited[index] = true;
 		
-		while(!q.isEmpty()) {
-			int parent = q.poll();
-			
-			for(int i : tree.get(parent)) {
-				if(!visited[i]) {
-					visited[i] = true;
-					parents[i] = parent;
-					q.add(i);
-				}
+		for(int i : list.get(index)) {
+			if(!visited[i]) {
+				parents[i] = index;
+				dfs(i);
 			}
 		}
 	}
