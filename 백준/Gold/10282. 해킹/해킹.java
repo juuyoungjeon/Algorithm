d9783e3fb20eb2cf3c8c254fe1c4c09af43aacaa
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-03-29 17:11:16 +0900

    [Gold IV] Title: 해킹, Time: 964 ms, Memory: 150900 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.java
+++ 백준/Gold/10282. 해킹/해킹.java
@@ -1,89 +1,103 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Computer implements Comparable<Computer> {
+    int depend; // 특정 컴퓨터를 의존하는 컴퓨터
+    int time;
+	
+    public Computer(int depend, int time) {
+        this.depend = depend;
+        this.time = time;
+    }
+
+    @Override
+    public int compareTo(Computer o) {
+        return this.time - o.time;
+    }
+}
 
 public class Main {
-	static class Node implements Comparable<Node>{
-		int idx;
-		int cost;
-		public Node(int idx, int cost) {
-			this.idx = idx;
-			this.cost = cost;
-		}
-		@Override
-		public int compareTo(Node o) {
-			// TODO Auto-generated method stub
-			return this.cost - o.cost;
-		}
-		
-	}
-	static int ans;
-	static int[] cost;
-	static StringBuilder sb = new StringBuilder();
-	static ArrayList<ArrayList<Node>> list;
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int tc = 1; tc <= T; tc++) {
-			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			int d = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			
-			ans = Integer.MAX_VALUE;
-			cost = new int[n + 1];
-			for(int i = 1; i <= n; i++) {
-				cost[i] = Integer.MAX_VALUE;
-			}
-			list = new ArrayList<>();
-			for(int i = 0; i <= n; i++) {
-				list.add(new ArrayList<>());
-			}
-			for(int i = 0; i < d; i++) {
-				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				int s = Integer.parseInt(st.nextToken());
-				
-				list.get(b).add(new Node(a, s));
-			}
-			dijkstra(c);
-			
-			ans = 0;
-			int cnt = 0;
-			for(int i = 1; i <= n; i++) {
-				if(cost[i] != Integer.MAX_VALUE) {
-					cnt++;
-					ans = Math.max(ans, cost[i]);
-				}
-			}
-			sb.append(cnt + " " + ans).append("\n");
-		}
-		System.out.println(sb);
-		
 
-	}
-	public static void dijkstra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.offer(new Node(start, 0));
-		cost[start] = 0;
+    static int INF = Integer.MAX_VALUE;
+	
+    static ArrayList<Computer>[] list;
+    static int[] dist;
+    static boolean[] visited;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		while(!pq.isEmpty()) {
-			Node now = pq.poll();
+        int T = Integer.parseInt(br.readLine());
+		
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 			
-			if(cost[now.idx] < now.cost) continue;
+            int n = Integer.parseInt(st.nextToken()); // 컴퓨터 개수
+            int d = Integer.parseInt(st.nextToken()); // 의존성 개수
+            int c = Integer.parseInt(st.nextToken()); // 해킹당한 컴퓨터의 번호
 			
-			for(Node next : list.get(now.idx)) {
-				if(cost[next.idx] > now.cost + next.cost) {
-					cost[next.idx] = now.cost + next.cost;
-					
-					pq.offer(new Node(next.idx, cost[next.idx]));
-				}
-			}
-		}
-	}
+            list = new ArrayList[n + 1];
+            dist = new int[n + 1];
+            visited = new boolean[n + 1];
+		
+            for (int i = 1; i < n + 1; i++) {
+                dist[i] = INF;
+                list[i] = new ArrayList<>();
+            }
+			
+            for (int i = 0; i < d; i++) {
+                st = new StringTokenizer(br.readLine());
+				
+                // 컴퓨터 a가 컴퓨터 b를 의존하며, 컴퓨터 b가 감염되면 s초 후 컴퓨터 a도 감염됨
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                int s = Integer.parseInt(st.nextToken());
+				
+                list[b].add(new Computer(a, s));
+            }
+			
+            dijkstra(c);
+			
+            int infection = 0; // 총 감염되는 컴퓨터 수
+            int total = 0; // 마지막 컴퓨터가 감염되기까지 걸리는 시간
+			
+            for (int i = 1; i < n + 1; i++) {
+                if (dist[i] != INF) { // dist[i]가 INF가 아닌 것은 감염된 컴퓨터
+                    infection++;
+                    total = Math.max(total, dist[i]);
+                }
+            }
+			
+            System.out.println(infection + " " + total);
+        }
+
+    }
+	
+    public static void dijkstra(int start) {
+        PriorityQueue<Computer> q = new PriorityQueue<>();
+		
+        dist[start] = 0;
+        q.offer(new Computer(start, 0));
+		
+        while (!q.isEmpty()) {
+            int cur = q.poll().depend;
+			
+            if (!visited[cur]) {
+                visited[cur] = true;
+				
+                for (Computer next : list[cur]) {
+                    if (dist[next.depend] > dist[cur] + next.time) {
+                        dist[next.depend] = dist[cur] + next.time;
+                        q.add(new Computer(next.depend, dist[next.depend]));
+                    }
+                }
+            }
+        }
+        
+    }
 
 }
