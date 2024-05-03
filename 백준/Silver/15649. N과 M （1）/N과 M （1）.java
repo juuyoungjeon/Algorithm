49c4c4ad365bf6b3ce30cc0481801fd6fe4bf15c
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-05-03 13:04:48 +0900

    [Silver III] Title: N과 M (1), Time: 2060 ms, Memory: 66212 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,41 +2,37 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
 	static int N, M;
-	static int[] result;
-	static boolean[] isSelected;
-	
-	public static void main(String[] args) throws IOException{
-
+	static int[] arr;
+	static boolean[] visited;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		result = new int[M];
-		isSelected = new boolean[N];
+		arr = new int[M];
+		visited = new boolean[N];
 		
-		perm(0);
+		dfs(0);
 	}
-
-	private static void perm(int cnt) {
-		if(cnt == M) {
+	public static void dfs(int depth) {
+		if(depth == M) {
 			for(int i = 0; i < M; i++) {
-				System.out.print(result[i]+" ");
+				System.out.print(arr[i] + " ");
 			}
 			System.out.println();
 			return;
 		}
 		
 		for(int i = 0; i < N; i++) {
-			if(!isSelected[i]) {
-				isSelected[i] = true;
-				result[cnt] = i + 1;
-				
-				perm(cnt + 1);
-				isSelected[i] = false;
+			if(!visited[i]) {
+				visited[i] = true;
+				arr[depth] = i+1;
+				dfs(depth+1);
+				visited[i] = false;
 			}
 		}
 	}
