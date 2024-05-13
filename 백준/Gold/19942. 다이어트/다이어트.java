94a9762398d9500801247517cf61229817a43542
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-05-13 14:17:49 +0900

    [Gold IV] Title: 다이어트, Time: 132 ms, Memory: 15700 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.java
+++ 백준/Gold/19942. 다이어트/다이어트.java
@@ -2,89 +2,77 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
 	static int N;
-	static int[][] arr;
-	static int result = Integer.MAX_VALUE;
-	static int[] min_nutrition;
-	static int[] isSelected;
-	static ArrayList<String> list;
-	
+	static int mp, mf, ms, mv;
+	static int[] p, f, s, v, c;
+	static int min = Integer.MAX_VALUE;
+	static ArrayList<Integer> list = new ArrayList<>();
+	static int[] ans;
+	static int[] selected;
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());
-		
-		arr = new int[N+1][5];
-		min_nutrition = new int[4];
-		list = new ArrayList<>();
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < 4; i++) {
-			min_nutrition[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		for(int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < 5; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		for(int i = 1; i <= N; i++) {
-			isSelected = new int[N];
-			dfs(0, i, 1);
-		}
-		if(result == Integer.MAX_VALUE) {
-			System.out.println(-1);
-		} else {
-			Collections.sort(list);
-			System.out.println(result);
-			System.out.println(list.get(0));
-		}
-	}
-
-	private static void dfs(int cnt, int choiceNum, int start) {
 		// TODO Auto-generated method stub
-		if(cnt == choiceNum) {
-			isCheck(choiceNum);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		N = Integer.parseInt(br.readLine());
+
+		st = new StringTokenizer(br.readLine());
+		mp = Integer.parseInt(st.nextToken());
+		mf = Integer.parseInt(st.nextToken());
+		ms = Integer.parseInt(st.nextToken());
+		mv = Integer.parseInt(st.nextToken());
+
+		p = new int[N];
+		f = new int[N];
+		s = new int[N];
+		v = new int[N];
+		c = new int[N];
+
+		ans = new int[N];
+		selected = new int[N];
+
+		Arrays.fill(ans, -1);
+		Arrays.fill(selected, -1);
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			p[i] = Integer.parseInt(st.nextToken());
+			f[i] = Integer.parseInt(st.nextToken());
+			s[i] = Integer.parseInt(st.nextToken());
+			v[i] = Integer.parseInt(st.nextToken());
+			c[i] = Integer.parseInt(st.nextToken());
+		}
+		dfs(0,0,0,0,0,0);
+
+		if(min == Integer.MAX_VALUE) {
+			System.out.println(-1);
 			return;
 		}
-		for(int i = start; i <= N; i++) {
-			isSelected[cnt] = i;
-			dfs(cnt +1, choiceNum, i +1);
-		}
-		
-	}
-
-	private static void isCheck(int choiceNum) {
-		int[] sum_nutrition = new int[4];
-		int price = 0;
-		
-		for(int i = 0; i < choiceNum; i++) {
-			for(int j = 0; j < 4; j++) {
-				sum_nutrition[j] += arr[isSelected[i]][j];
+		sb.append(min).append("\n");
+		for(int i = 0; i < N; i++) {
+			if(ans[i] != -1) {
+				sb.append(ans[i] + 1).append(" ");
 			}
-			price += arr[isSelected[i]][4];
 		}
-		for(int i=0;i<4;i++)
-            if(min_nutrition[i] > sum_nutrition[i]) return ; //최소비용 만족 못할 시 return
-            
-            if(result >= price){
-                //이전에 선택한 가격 보다 지금 선택한 것이 더 싸다면
-                //갱신해주기
-                if(result > price) list.clear();
-                StringBuilder sb = new StringBuilder();
-                for(int i=0;i<choiceNum;i++){
-                    sb.append(isSelected[i]+" ");
-                }
+		System.out.println(sb);
 
-                list.add(sb.toString());
-                result = price;
-            }
-
-
-    }
+	}
+	public static void dfs(int depth, int protein, int fat, int carbon, int vitamin, int cost) {
+		if(protein >= mp && fat >= mf && carbon >= ms && vitamin >= mv && cost < min) {
+			for(int i = 0; i < N; i++) {
+				ans[i] = selected[i];
+			}
+			min = cost;
+//			min = Math.min(min, cost);
+			return;
+		}
+		if(depth == N) {
+			return;
+		}
+		selected[depth] = depth;
+		dfs(depth + 1, protein + p[depth], fat + f[depth], carbon + s[depth], vitamin + v[depth], cost + c[depth]);
+		selected[depth] = -1;
+		dfs(depth + 1, protein, fat, carbon, vitamin, cost);
+	}
 
 }
