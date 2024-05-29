928f79c833184cbc33d02b36a9bcd95a9cc810df
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-05-29 22:26:50 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 572 ms, Memory: 53116 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -2,47 +2,33 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
 	static int N, M;
-	static long[] tree;
+	static int[] arr;
 	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		tree = new long[N+1];
+		
+		arr = new int[N+1];
 		
 		st = new StringTokenizer(br.readLine());
-		for(int i = 1; i < N + 1; i++) {
-			update(i, Long.parseLong(st.nextToken()));
+		for (int i = 1; i <= N; i++) {
+			arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
 		}
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < M; i++) {
+		
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			long s = sum(a,b);
-			sb.append(s).append("\n");
+			
+			sb.append(arr[b] - arr[a-1]).append("\n");
 		}
-		System.out.println(sb.toString());
-	}
-	private static void update(int i, long diff) {
-		while(i < N + 1) {
-			tree[i] += diff;
-			i += (i & -i);
-		}
-	}
-	private static long sum(int i) {
-		long result = 0;
-		while(i > 0) {
-			result += tree[i];
-			i -= (i & -i);
-		}
-		return result;
-	}
-	private static long sum(int i, int j) {
-		return sum(j) - sum(i - 1);
+		
+		System.out.println(sb);
 	}
 
 }
