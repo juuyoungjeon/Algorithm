76b1bc791cf7d2ebfa6bc7d71c0d4e5d57ec076b
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-06-03 19:57:12 +0900

    [Silver II] Title: 연속합, Time: 264 ms, Memory: 23252 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -2,30 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
+	static int N;
+	static int[] arr;
+	static int[] dp;
+	static int max;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		int[] arr = new int[N + 1];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 1; i <= N; i++) {
-			arr[i] =  Integer.parseInt(st.nextToken());
+		StringTokenizer st;
+
+		N = Integer.parseInt(br.readLine());
+
+		arr = new int[N];
+		dp = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		dp[0] = arr[0];
+		max = arr[0];
+		for(int i = 1; i < N; i++) {
+			dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
+			max = Math.max(max, dp[i]);
 		}
 		
-		int[] dp = new int[N + 1];
-		dp[1] = arr[1];
-		for(int i = 2; i <= N; i++) {
-			dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
-		}
-		
-		int max = Integer.MIN_VALUE;
-		for(int i = 1; i <= N; i++) {
-			max = Math.max(dp[i], max);
-		}
 		System.out.println(max);
 	}
 
