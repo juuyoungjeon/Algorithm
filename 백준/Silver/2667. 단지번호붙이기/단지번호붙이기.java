958fa350b3aa39d46db90d550c5819eb36af9cfe
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-07-01 17:56:06 +0900

    [Silver I] Title: 단지번호붙이기, Time: 124 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,67 +2,66 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N, cnt;
-	static int[][] map, copy;
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
+	static int N;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dx = {-1,0,1,0};
+	static int[] dy = {0,-1,0,1};
+	static int num;
+	static int cnt;
 	static ArrayList<Integer> list;
-	static int[][] visited;
-	static int[] dongCnt;
 	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
 		N = Integer.parseInt(br.readLine());
-		
+
 		map = new int[N][N];
-		copy = new int[N][N];
-		visited = new int[N][N];
+		visited = new boolean[N][N];
 		list = new ArrayList<>();
 		
 		for(int i = 0; i < N; i++) {
 			String s = br.readLine();
 			for(int j = 0; j < N; j++) {
 				map[i][j] = s.charAt(j) - '0';
-				
 			}
 		}
-		int dong=1;
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				if(map[i][j]!=0 && visited[i][j]==0) {
-					dfs(i,j,dong);
-					dong++;
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				if(map[i][j] == 1 && !visited[i][j]) {
+					cnt = 0;
+					num++;
+					dfs(i,j);
+					list.add(cnt);
 				}
 			}
 		}
-		dongCnt=new int[dong];
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				if(visited[i][j]==0) continue;
-				dongCnt[visited[i][j]]+=1;
-			}
+		
+		Collections.sort(list);
+		sb.append(num).append("\n");
+		for(int i : list) {
+			sb.append(i + "\n");
 		}
-		dongCnt[0]=999999999;
-		Arrays.sort(dongCnt);
-		System.out.println(dongCnt.length-1);
-		for(int i=0;i<dongCnt.length-1;i++) {
-			System.out.println(dongCnt[i]);
-		}
+		
+		System.out.println(sb);
+		
 	}
-	
-	public static void dfs(int x, int y,int dong) {
-		visited[x][y] = dong;
+	public static void dfs(int x, int y) {
+		visited[x][y] = true;
+		map[x][y] = num;
+		cnt++;
 		
 		for(int i = 0; i < 4; i++) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 			
-			if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-			if(visited[nx][ny]!=0) continue;
-			if(map[nx][ny] != 0 && visited[nx][ny]==0) {
-				visited[nx][ny]=dong;
-				dfs(nx,ny,dong);
-			}
+			if(nx < 0 || nx >= N || ny < 0 || ny >= N || visited[nx][ny]) continue;
+			if(map[nx][ny] == 0) continue;
+			visited[nx][ny] = true;
+			map[nx][ny] = num;
+			dfs(nx, ny);
 		}
 	}
 
