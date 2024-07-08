50cf6f9da8963e940e06a3af2fac901c398434d1
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-07-08 13:25:15 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 388 ms, Memory: 37888 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -3,18 +3,22 @@ import java.io.*;
 
 public class Main {
 	static int N, K;
-	static int[] time = new int[100001], parent = new int[100001];
-	
+	static int[] time;
+	static int[] parent;
 	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
+
+		time = new int[100001];
+		parent = new int[100001];
 		
-		bfs();
-		
+		solve();
 		Stack<Integer> s = new Stack<>();
 		s.push(K);
 		
@@ -31,40 +35,34 @@ public class Main {
 			sb.append(s.pop() + " ");
 		}
 		
-		System.out.println(sb.toString());
-		
-		
+		System.out.println(sb);
 	}
-	public static void bfs() {
+	public static void solve() {
 		Queue<Integer> q = new LinkedList<>();
 		q.add(N);
 		time[N] = 1;
 		
 		while(!q.isEmpty()) {
-			
 			int now = q.poll();
-			
 			if(now == K) return;
-			
-			for(int i = 0; i <= 3; i++) {
-				int nx;
+			for(int i = 0; i < 3; i++) {
+				int nx = 0;
 				if(i == 0) {
-					nx = now -1;
+					nx = now - 1;
 				}else if(i == 1) {
 					nx = now + 1;
 				}else {
-					nx = now*2;
+					nx = now * 2;
 				}
-				
+
 				if(nx < 0 || nx > 100000) continue;
-				
+
 				if(time[nx] == 0) {
 					q.add(nx);
 					time[nx] = time[now] + 1;
 					parent[nx] = now;
 				}
 			}
-			
 		}
 	}
 
