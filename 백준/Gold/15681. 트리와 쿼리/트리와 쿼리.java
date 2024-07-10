37a5e756563613cba61bf8530387a6161e8945c2
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-07-10 19:54:12 +0900

    [Gold V] Title: 트리와 쿼리, Time: 804 ms, Memory: 83596 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -1,22 +1,24 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main {
-	static int N, R, Q;
-	static ArrayList<ArrayList<Integer>> list;
+	static int N,R,Q;
 	static boolean[] visited;
-	static StringBuilder sb = new StringBuilder();
-	static int cnt = 0;
 	static int[] dp;
+	static ArrayList<ArrayList<Integer>> list;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
 		Q = Integer.parseInt(st.nextToken());
 		
+		visited = new boolean[N+1];
+		dp = new int[N+1];
 		list = new ArrayList<>();
 		for(int i = 0; i <= N; i++) {
 			list.add(new ArrayList<>());
@@ -24,39 +26,35 @@ public class Main {
 		
 		for(int i = 0; i < N - 1; i++) {
 			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
 			
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			list.get(a).add(b);
-			list.get(b).add(a);
+			list.get(u).add(v);
+			list.get(v).add(u);
 		}
 		
-		dp = new int[N + 1];
-		visited = new boolean[N + 1];
 		visited[R] = true;
-		dfs(R);
-
+		solve(R);
 		
 		for(int i = 0; i < Q; i++) {
-			int node = Integer.parseInt(br.readLine());
-			
-			sb.append(dp[node]).append("\n");
+			sb.append(dp[Integer.parseInt(br.readLine())]).append("\n");
 		}
-
+		
 		System.out.println(sb);
+		
 	}
-	
-	public static int dfs(int index) {
+	public static int solve(int idx) {
+		visited[idx] = true;
 		int cnt = 1;
-		for(int i : list.get(index)) {
+		for(int i : list.get(idx)) {
 			if(!visited[i]) {
 				visited[i] = true;
-				cnt += dfs(i);
+				cnt += solve(i);
 			}
 		}
 		
-		return dp[index] = cnt;
+		return dp[idx] = cnt;
+		
 	}
 
 }
