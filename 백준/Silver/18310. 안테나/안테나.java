b1fd738ed2402df0f469a2c5722bfe137c1724dc
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-08-07 14:47:43 +0900

    [Silver III] Title: 안테나, Time: 500 ms, Memory: 40236 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N;
+	static int[] arr;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		
+		arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[(N-1)/2]);
+
+	}
+
+}
