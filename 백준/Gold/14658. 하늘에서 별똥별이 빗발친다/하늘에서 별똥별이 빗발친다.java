a4dba694a9c6b25bfcf1bd2da07455201c5746cd
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-08-19 14:40:02 +0900

    [Gold III] Title: 하늘에서 별똥별이 빗발친다, Time: 176 ms, Memory: 16196 KB -BaekjoonHub

--- 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.java
+++ 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.java
@@ -4,8 +4,8 @@ import java.io.*;
 public class Main {
 	static int N,M,L,K;
 	static int[][] map;
-	static int[] arr;
-	static int max;
+	static int max = Integer.MIN_VALUE;
+	static int cnt;
 	static class Node{
 		int x;
 		int y;
@@ -14,41 +14,46 @@ public class Main {
 			this.y = y;
 		}
 	}
-	static ArrayList<Node> list = new ArrayList<>();
+	static ArrayList<Node> list;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		L = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-
-
+		
+//		map = new int[N+1][M+1];
+		list = new ArrayList<>();
+		
 		for(int i = 0; i < K; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-
-			list.add(new Node(a,b));
-
+			
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+//			map[x][y] = 1;
+			list.add(new Node(x, y));
 		}
-
+		
+//		for(int i = 1; i <= N; i++) {
+//			for(int j = 1; j <= M; j++) {
 		for(Node n1 : list) {
 			for(Node n2 : list) {
-				int cnt = 0;
-				for (Node node : list) {
-					if (n1.x <= node.x && node.x <= n1.x + L && n2.y <= node.y && node.y <= n2.y + L) {
+				cnt = 0;
+				for(Node node : list) {
+					if(n1.x <= node.x && node.x <= n1.x + L
+							&& n2.y <= node.y && node.y <= n2.y + L) {
 						cnt++;
 					}
-					max = Math.max(cnt, max);
+					max = Math.max(max, cnt);
 				}
 			}
 		}
 		
 		System.out.println(K - max);
-
 	}
 
 }
