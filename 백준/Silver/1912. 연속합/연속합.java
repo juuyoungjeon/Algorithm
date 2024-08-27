47848b2dc1917c2d17edbaad3aea6fd8ff7b3f94
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-08-27 16:45:21 +0900

    [Silver II] Title: 연속합, Time: 240 ms, Memory: 24044 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -4,30 +4,29 @@ import java.io.*;
 public class Main {
 	static int N;
 	static int[] arr;
-	static int[] dp;
 	static int max;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-
+		
 		N = Integer.parseInt(br.readLine());
-
+		
 		arr = new int[N];
-		dp = new int[N];
-
+		int[] dp = new int[N];
+		
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-
+		
 		dp[0] = arr[0];
 		max = arr[0];
+		
 		for(int i = 1; i < N; i++) {
-			dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
+			dp[i] = Math.max(arr[i], dp[i-1] + arr[i]);
 			max = Math.max(max, dp[i]);
 		}
-		
 		System.out.println(max);
 	}
 
