20206663e7bcfd379be7eb9c8cc3279a45f803b1
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-08-27 13:01:52 +0900

    [Gold IV] Title: 점수따먹기, Time: 720 ms, Memory: 21892 KB -BaekjoonHub

--- 백준/Gold/1749. 점수따먹기/점수따먹기.java
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -3,43 +3,44 @@ import java.io.*;
 
 public class Main {
 	static int N, M;
-	static int[][] arr;
+	static int[][] map;
 	static int[][] prefixSum;
+	static int max = Integer.MIN_VALUE;
 	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		arr = new int[N+1][M+1];
-		prefixSum = new int[N+1][M+1];
-		
+		map = new int[N+1][M+1];
 		for(int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 1; j <= M; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		prefixSum = new int[N+1][M+1];
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= M; j++) {
+				prefixSum[i][j] = prefixSum[i][j-1] + prefixSum[i-1][j] - prefixSum[i-1][j-1] + map[i][j];
 			}
 		}
 		
 		for(int i = 1; i <= N; i++) {
 			for(int j = 1; j <= M; j++) {
-				prefixSum[i][j] = prefixSum[i][j-1] + prefixSum[i-1][j] - prefixSum[i-1][j-1] + arr[i][j];
-			}
-		}
-		
-		int max = Integer.MIN_VALUE;
-		for(int i = 1; i <= N; i++) {
-			for(int j = 1; j <= M; j++) {
-				for(int k = i; k > 0; k--) {
-					for(int l = j; l > 0; l--) {
-						max = Math.max(prefixSum[i][j] - prefixSum[k-1][j] - prefixSum[i][l-1] + prefixSum[k-1][l-1], max);
+				for(int k = 1; k <= i; k++) {
+					for(int l = 1; l <= j; l++) {
+						max = Math.max(max, prefixSum[i][j] - prefixSum[k-1][j] - prefixSum[i][l-1] + prefixSum[k-1][l-1]);
 					}
 				}
 			}
 		}
 		
 		System.out.println(max);
+		
 	}
 
 }
