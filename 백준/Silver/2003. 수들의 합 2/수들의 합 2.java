9da26b36492dc06ac91da41a963e9f363c3ae3c6
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-08-28 16:12:29 +0900

    [Silver IV] Title: 수들의 합 2, Time: 128 ms, Memory: 15268 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,56 +1,41 @@
-import java.io.BufferedReader;
 import java.util.*;
 import java.io.*;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException{
+	static int N, M;
+	static int[] arr;
+	static int cnt;
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
-		int[] array = new int[N+1];
-		
+		arr = new int[N+1];
 		st = new StringTokenizer(br.readLine());
-		
-		for(int i=1; i<N+1; i++) {
-			array[i] = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int R=1;
-		int sum=array[1];
-		int ans=0;
-		
-		if(sum == M) {
-			ans++;
-		}
-		
-		for(int L=1;L<N+1; L++) {
-			while(R<N) {
-				if(sum+array[R+1]> M) {
-					break;
-				}
-				if(sum+array[R+1]==M) {
-					ans++;
-					break;
-				}
-				if(sum+array[R+1]<M){
-					R++;
-					sum=sum+array[R];
-				}
+		int s = 0;
+		int e = 0;
+		int total = 0;
+		while(e <= N) {
+			if(total >= M) {
+				total -= arr[s++];
+			}else {
+				total += arr[e++];
 			}
-			sum=sum-array[L];
-			if(sum==M) {
-				ans++;
+			
+			if(total == M) {
+				cnt++;
 			}
 		}
-		System.out.println(ans);
 		
-
+		System.out.println(cnt);
+		
 	}
 
 }
