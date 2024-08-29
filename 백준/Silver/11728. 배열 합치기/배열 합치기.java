f7e8135ef04de1a561340b3a9d7c1a69441bbf49
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-08-29 22:06:36 +0900

    [Silver V] Title: 배열 합치기, Time: 1492 ms, Memory: 349884 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -2,35 +2,34 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
+	static int N, M;
+	static ArrayList<Integer> list = new ArrayList<>();
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		int[] C = new int[N+M];
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
-			C[i] = Integer.parseInt(st.nextToken());
+			list.add(Integer.parseInt(st.nextToken()));
 		}
 		
 		st = new StringTokenizer(br.readLine());
-		for(int i = N; i < N + M; i++) {
-			C[i] = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < M; i++) {
+			list.add(Integer.parseInt(st.nextToken()));
 		}
 		
-		Arrays.sort(C);
+		Collections.sort(list);
 		
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < N + M; i++) {
-			sb.append(C[i] + " ");
+		for(int i : list) {
+			sb.append(i + " ");
 		}
+		
 		System.out.println(sb);
-
 	}
 
 }
