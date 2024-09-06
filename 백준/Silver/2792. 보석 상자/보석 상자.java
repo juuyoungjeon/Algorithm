7117fd45a07937afab6e497e0ec970f0b98d901c
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-09-06 12:51:15 +0900

    [Silver I] Title: 보석 상자, Time: 332 ms, Memory: 34720 KB -BaekjoonHub

--- 백준/Silver/2792. 보석 상자/보석 상자.java
+++ 백준/Silver/2792. 보석 상자/보석 상자.java
@@ -1,44 +1,60 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-public static void main(String[] args) throws IOException {
-	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	StringTokenizer st = new StringTokenizer(in.readLine(), " ");
-
-	int N = Integer.parseInt(st.nextToken());
-	int M = Integer.parseInt(st.nextToken());
-	int arr[] = new int[M];
-	int left = 1, right = 0, mid = 0, sum, answer = 0;
-
-	// 각 색상별 보석의 개수
-	for (int i = 0; i < M; i++) {
-		arr[i] = Integer.parseInt(in.readLine());
-		right = Math.max(right, arr[i]);
+	static int N, M;
+	static int[] arr;
+	static int cnt;
+	static int s,e;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[M];
+		
+		for(int i = 0; i < M; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			e = Math.max(arr[i], e);
+		}
+		
+		s = 1;
+		paramatricSearch();
+		
+		System.out.println(s);
 	}
-
-	// 이분 탐색 하면서 최솟값 찾기
-	while (left <= right) {
-		// 질투심이 mid가 되도록 보석 나눠주기
-		mid = (left + right) / 2;
-		sum = 0;
-		for (int i = 0; i < M; i++) {
-			sum += arr[i] / mid;
-			if (arr[i] % mid != 0) {
-				sum++;
+	/*
+	 * 파라메트릭 서치에서 매개변수는 "나눠줄 보석의 수"
+	 * 매개변수로 나눠줬을때 총 몇명의 사람에게 나눠줄 수 있는지 계산하고
+	 * 계산했을때 사람의 수가 N보다 크면, 나눠주는 보석의 수를 증가시켜 사람 수 줄여준다
+	 * 질투심이 최소가 되게 보석을 나눠주기 위해서는 보석의 수를 최대한 감소시켜야 함
+	 * 
+	 * 한명한테 보석을 모두 나눠주면 질투심이 높아지기 때문에
+	 * 최대한 많은 사람아게 동일하게 보석을 나눠줘야함
+	 * 
+	 * */
+	public static void paramatricSearch() {
+		while(s<=e) {
+			int mid = (s + e) / 2;
+			int people = 0;
+			
+			for(int i = 0; i < M; i++) {
+				if(arr[i] % mid == 0) {
+					people += arr[i]/mid;
+				}else {
+					people += arr[i]/mid + 1;
+				}
+			}
+			
+			if(people > N) {
+				s = mid + 1;
+			}else {
+				e = mid - 1;
 			}
 		}
-		// 보석을 나눠줄 수 없는 경우
-		if (sum > N) {
-			left = mid + 1;
-		}
-		// 보석을 나눠줄 수 있는 경우
-		else {
-			right = mid - 1;
-			answer = mid;
-		}
+		
 	}
-	
-	// 질투심의 최솟값 출력
-	System.out.println(answer);
-}
 }
