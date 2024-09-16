b063eb04ca6c5aca4da528499fccaba0c19d0331
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-09-16 17:37:05 +0900

    [Silver III] Title: N과 M (6), Time: 116 ms, Memory: 14676 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,45 +1,42 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N;
-	static int M;
-	static boolean[] visited;
+	static int N, M;
 	static int[] arr;
-	static int[] input;
+	static int[] res;
+	static boolean[] visited;
 	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		input = new int[N];
+		arr = new int[N];
+		visited = new boolean[N];
+		res = new int[M];
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
-			input[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		Arrays.sort(input);
+		Arrays.sort(arr);
+		solve(0, 0);
 		
-		visited = new boolean[N];
-		arr = new int[M];
-		dfs(0,0);
 		System.out.println(sb);
-		
 	}
-
-	private static void dfs(int depth, int start) {
-		// TODO Auto-generated method stub
+	public static void solve(int depth, int start) {
 		if(depth == M) {
-			for(int val : arr) {
-				sb.append(val).append(" ");
+			for(int i = 0; i < M; i++) {
+				sb.append(res[i] + " ");
 			}
 			sb.append("\n");
 			return;
@@ -48,11 +45,12 @@ public class Main {
 		for(int i = start; i < N; i++) {
 			if(!visited[i]) {
 				visited[i] = true;
-				arr[depth] = input[i];
-				dfs(depth + 1, i);
+				res[depth] = arr[i];
+				solve(depth + 1, i + 1);
 				visited[i] = false;
 			}
 		}
 	}
+	
 
 }
