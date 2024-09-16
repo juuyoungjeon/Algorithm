11531e3f8a0f4e4eb939b87f56ec9e2d3ecb823a
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-09-16 17:04:31 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 15836 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -3,10 +3,10 @@ import java.io.*;
 
 public class Main {
 	static int N, M;
-	static int[] result;
+	static int[] res;
 	static boolean[] visited;
 	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,18 +14,18 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		result = new int[M];
+		res = new int[M];
 		visited = new boolean[N];
 		
-		permutation(0,0);
+		solve(0, 0);
 		
 		System.out.println(sb);
+		
 	}
-	
-	public static void permutation(int depth, int start) {
+	public static void solve(int depth, int start) {
 		if(depth == M) {
 			for(int i = 0; i < M; i++) {
-				sb.append(result[i] + " ");
+				sb.append(res[i] + " ");
 			}
 			sb.append("\n");
 			return;
@@ -34,8 +34,8 @@ public class Main {
 		for(int i = start; i < N; i++) {
 			if(!visited[i]) {
 				visited[i] = true;
-				result[depth] = i + 1;
-				permutation(depth + 1, i + 1);
+				res[depth] = i + 1;
+				solve(depth + 1, i);
 				visited[i] = false;
 			}
 		}
