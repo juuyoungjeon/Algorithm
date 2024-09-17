b5e496a7136b83082f50be25a4d120e5549dfa83
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-09-17 22:34:39 +0900

    [Silver II] Title: 차이를 최대로, Time: 124 ms, Memory: 15432 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -1,44 +1,47 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[] arr, result;
+	static int N;
+	static int[] arr, res;
 	static boolean[] visited;
-	static int N, ans;
+	static int max = Integer.MIN_VALUE;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
 		
 		arr = new int[N];
+		res = new int[N];
 		visited = new boolean[N];
-		result = new int[N];
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		dfs(0);
-		System.out.println(ans);
+		solve(0);
+		System.out.println(max);
 	}
-	public static void dfs(int depth) {
+	static int sum;
+	public static void solve(int depth) {
 		if(depth == N) {
-			int sum = 0;
+			sum = 0;
 			for(int i = 0; i < N - 1; i++) {
-				sum += Math.abs(result[i] - result[i + 1]);
+				sum += Math.abs(res[i] - res[i+1]);
 			}
-			ans = Math.max(ans, sum);
+			max = Math.max(max, sum);
 			return;
 		}
 		
 		for(int i = 0; i < N; i++) {
 			if(!visited[i]) {
 				visited[i] = true;
-				result[depth] = arr[i];
-				dfs(depth + 1);
+				res[depth] = arr[i];
+				solve(depth + 1);
 				visited[i] = false;
 			}
 		}
