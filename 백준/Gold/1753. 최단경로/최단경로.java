84c125f406b6dd220e6c333804536f5d6271ab54
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-09-24 19:47:00 +0900

    [Gold IV] Title: 최단경로, Time: 644 ms, Memory: 109092 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,75 +2,75 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	static int V, E, K;
 	static int[] dist;
-	static int v, e, k;
+	static int min = Integer.MAX_VALUE;
+	static ArrayList<ArrayList<Node>> list;
 	static class Node implements Comparable<Node>{
 		int idx;
-		int cost;
-		public Node(int idx, int cost) {
+		int value;
+		
+		public Node(int idx, int value) {
 			this.idx = idx;
-			this.cost = cost;
+			this.value = value;
 		}
+		
 		@Override
 		public int compareTo(Node o) {
-			// TODO Auto-generated method stub
-			return this.cost - o.cost;
+			return this.value - o.value;
 		}
 	}
-	static ArrayList<ArrayList<Node>> list;
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		v = Integer.parseInt(st.nextToken());
-		e = Integer.parseInt(st.nextToken());
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
 		
-		k = Integer.parseInt(br.readLine());
+		K = Integer.parseInt(br.readLine());
 		
 		list = new ArrayList<>();
-		for(int i = 0; i <= v; i++) {
+		for(int i = 0; i <= V; i++) {
 			list.add(new ArrayList<>());
 		}
 		
-		dist = new int[v + 1];
-		for(int i = 1; i <= v; i++) {
+		dist = new int[V+1];
+		for(int i = 1; i <= V; i++) {
 			dist[i] = Integer.MAX_VALUE;
 		}
 		
-		for(int i = 0; i < e; i++) {
+		for(int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
 			
-			list.get(a).add(new Node(b, c));
+			list.get(u).add(new Node(v,w));
 		}
 		
-		bfs();
-		
-		for(int i = 1; i <= v; i++) {
+		dijkstra(K);
+		for(int i = 1; i <= V; i++) {
 			sb.append(dist[i] != Integer.MAX_VALUE? dist[i] : "INF").append("\n");
 		}
 		
 		System.out.println(sb);
-		
 	}
-	public static void bfs() {
+	public static void dijkstra(int start) {
 		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(k, 0));
-		dist[k] = 0;
+		pq.add(new Node(start, 0));
+		dist[start] = 0;
 		
 		while(!pq.isEmpty()) {
 			Node now = pq.poll();
-			if(dist[now.idx] < now.cost) continue;
+			if(dist[now.idx] < now.value) continue;
 			
 			for(int i = 0; i < list.get(now.idx).size(); i++) {
 				Node next = list.get(now.idx).get(i);
 				
-				if(dist[next.idx] > now.cost + next.cost) {
-					dist[next.idx] = now.cost + next.cost;
+				if(dist[next.idx] > now.value + next.value) {
+					dist[next.idx] = now.value + next.value;
 					pq.add(new Node(next.idx, dist[next.idx]));
 				}
 			}
