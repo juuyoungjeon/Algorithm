b531016827cc286d35d6ea11ffe2ca60c2bfdd13
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-10-01 17:41:48 +0900

    [Silver II] Title: 영재의 시험, Time: 268 ms, Memory: 16036 KB -BaekjoonHub

--- 백준/Silver/19949. 영재의 시험/영재의 시험.java
+++ 백준/Silver/19949. 영재의 시험/영재의 시험.java
@@ -2,68 +2,45 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int[] ans, arr, visited;
-	static int cnt, res;
-	static StringBuilder sb = new StringBuilder();
+	static int ans, cnt;
+	static int[] test, arr;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		ans = new int[10];
+
+		test = new int[10];
 		arr = new int[10];
-		
-		for (int i = 0; i < 10; i++) {
-			ans[i] = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < 10; i++) {
+			test[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		cnt = 0;
-		res = 0;
+
 		dfs(0);
-		System.out.println(res);
-//		check(0);
-//		System.out.println(sb);
-		
+		System.out.println(ans);
+
 	}
-	
 	public static void dfs(int depth) {
-		if (depth == 10) {
+		if(depth == 10) {
 			cnt = 0;
 			for(int i = 0; i < 10; i++) {
-				if(arr[i] == ans[i]) {
+				if(arr[i] == test[i]){
 					cnt++;
 				}
 				if(cnt == 5) {
-					res++;
+					ans++;
 					return;
 				}
 			}
 			return;
 		}
-		
 		for(int i = 1; i <= 5; i++) {
 			if(depth > 1) {
-				if(arr[depth-2] == arr[depth - 1] && arr[depth - 1] == i) continue;
+				if(arr[depth - 2] == arr[depth - 1] && arr[depth - 1] == i) continue;
 			}
 			arr[depth] = i;
 			dfs(depth + 1);
 		}
 		
-	}
-	
-	public static void check(int depth) {
-		if(depth == 10) {
-			for(int i = 0; i < 10; i++) {
-				sb.append(arr[i] + " ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for(int i = 1; i <= 5; i++) {
-			arr[depth] = i;
-			check(depth + 1);
-		}
 	}
 
 }
