0b2c7ba16fa58db0b5dc207e8bb2bc6df3b9fe97
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-10-04 23:15:22 +0900

    [Gold V] Title: 퇴사 2, Time: 632 ms, Memory: 310256 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -3,8 +3,7 @@ import java.io.*;
 
 public class Main {
 	static int N, max;
-	static int[] T,P;
-	static int[] DP;
+	static int[] T, P, DP;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,7 +14,7 @@ public class Main {
 		T = new int[N + 2];
 		P = new int[N + 2];
 		
-		for(int i = 1; i <= N; i++) {
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			T[i] = Integer.parseInt(st.nextToken());
 			P[i] = Integer.parseInt(st.nextToken());
@@ -23,19 +22,18 @@ public class Main {
 		
 		DP = new int[N + 2];
 		
-		for(int i = 1; i < N + 2; i++ ) {
-			if(max < DP[i]) {
+		max = -1;
+		for (int i = 1; i < N + 2; i++) {
+			if (max < DP[i]) {
 				max = DP[i];
 			}
-			
-			int day = i + T[i];
-			if(day < N + 2) {
-				DP[day] = Math.max(DP[day], max + P[i]);
+			int next = i + T[i];
+			if (next < N + 2) {
+				DP[next] = Math.max(DP[next], max + P[i]);
 			}
 		}
 		
 		System.out.println(max);
-
 	}
 
 }
