0bb009deaa42cd41eca5f80f9a069076a87ae6de
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-10-14 15:24:25 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 432 ms, Memory: 37508 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -3,20 +3,20 @@ import java.io.*;
 
 public class Main {
 	static int N;
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
 	static int[][] map;
 	static int[][] dp;
-	
+	static int max;
+	static int[] dx = {-1,0,1,0};
+	static int[] dy = {0,-1,0,1};
+	static boolean[][] visited;
 	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
 		StringTokenizer st;
 		
 		N = Integer.parseInt(br.readLine());
 		
 		map = new int[N][N];
-		dp = new int[N][N];
 		
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -24,21 +24,24 @@ public class Main {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+		visited = new boolean[N][N];
 		
-		int ans = 0;
+		dp = new int[N][N];
+		for(int[] i : dp) {
+			Arrays.fill(i, -1);
+		}
+		
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < N; j++) {
-				ans = Math.max(ans, DFS(i,j));
+				max = Math.max(max, solve(i,j));
 			}
 		}
-		System.out.println(ans);
+		
+		System.out.println(max);
 	}
 
-	private static int DFS(int x, int y) {
-		// TODO Auto-generated method stub
-		if(dp[x][y] != 0) {
-			return dp[x][y];
-		}
+	public static int solve(int x, int y) {
+		if(dp[x][y] != -1) return dp[x][y];
 		
 		dp[x][y] = 1;
 		
@@ -46,16 +49,13 @@ public class Main {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 			
-			if(nx < 0 || ny < 0 || nx >= N || ny >= N) {
-				continue;
-			}
-			
-			if(map[x][y] < map[nx][ny]) {
-				dp[x][y] = Math.max(dp[x][y], DFS(nx, ny) + 1);
-				DFS(nx, ny);
+			if(nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny] && map[nx][ny] > map[x][y]) {
+				visited[nx][ny] = true;
+				dp[x][y] = Math.max(dp[x][y], solve(nx, ny) + 1);
+				visited[nx][ny] = false;
 			}
 		}
+		
 		return dp[x][y];
 	}
-
 }
