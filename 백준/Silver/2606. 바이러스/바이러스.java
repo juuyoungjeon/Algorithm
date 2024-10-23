062f02fb65cb85db4100adee3ffacd723768e984
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-10-23 17:12:28 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -3,42 +3,42 @@ import java.io.*;
 
 public class Main {
 	static int N, M;
-	static int ans;
-	static boolean[] visited;
 	static ArrayList<ArrayList<Integer>> list;
+	static boolean[] visited;
+	static int ans;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
+
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
-		
+
 		list = new ArrayList<>();
 		for(int i = 0; i <= N; i++) {
 			list.add(new ArrayList<>());
 		}
-		
-		visited = new boolean[N+1];
 		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
+
 			list.get(a).add(b);
 			list.get(b).add(a);
 		}
-		dfs(1);
+		visited = new boolean[N+1];
+
+		solve(1);
+
 		System.out.println(ans);
-		
 	}
-	public static void dfs(int start) {
-		visited[start] = true;
-		for(int a : list.get(start)) {
+	public static void solve(int depth) {
+		visited[depth] = true;
+		for(int a : list.get(depth)) {
 			if(!visited[a]) {
 				visited[a] = true;
 				ans++;
-				dfs(a);
+				solve(a);
 			}
 		}
 	}
