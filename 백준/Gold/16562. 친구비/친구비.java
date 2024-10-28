bd9f188e1522814cd52d4cf59ad62fc38244181e
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-10-28 17:59:51 +0900

    [Gold IV] Title: 친구비, Time: 220 ms, Memory: 22208 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.java
+++ 백준/Gold/16562. 친구비/친구비.java
@@ -3,10 +3,12 @@ import java.io.*;
 
 public class Main {
 	static int n, m, k;
+	static int min;
+	static int[] arr;
 	static boolean[] visited;
-	static int[] cost, parent;
-	static int v,w;
 	static ArrayList<ArrayList<Integer>> list;
+	static int cost = Integer.MAX_VALUE;
+	static ArrayList<Integer> money;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,68 +17,67 @@ public class Main {
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
-
+		
+		arr = new int[n+1];
+		visited = new boolean[n+1];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 		list = new ArrayList<>();
 		for(int i = 0; i <= n; i++) {
 			list.add(new ArrayList<>());
 		}
-		
-		visited = new boolean[n + 1];
-		cost = new int[n + 1];
-		parent = new int[n + 1];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 1; i <= n; i++) {
-			cost[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		for(int i = 0; i <= n; i++) {
-			parent[i] = i;
-		}
-		
 		for(int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 			
-			v = Integer.parseInt(st.nextToken());
-			w = Integer.parseInt(st.nextToken());
-			
-			if(v > w) {
-				union(w, v);
-			}else {
-				union(v, w);
-			}
+			list.get(a).add(b);
+			list.get(b).add(a);
 		}
 		
-		long cnt = 0;
 		for(int i = 1; i <= n; i++) {
-			int idx = find(i);
-			
-			if(!visited[idx]) {
-				cnt += cost[idx];
-				visited[idx] = true;
+			if(!visited[i]) {
+				min += dfs(i);
 			}
-			
-			visited[i] = true;
 		}
 		
-		System.out.println(cnt> k? "Oh no" : cnt);
-	}
-	
-	public static void union(int a, int b) {
-		a = find(a);
-		b = find(b);
-		if(cost[a] > cost[b]) {
-			parent[a] = b;
+		if(min <= k) {
+			System.out.println(min);
 		}else {
-			parent[b] = a;
+			System.out.println("Oh no");
 		}
+
 	}
-	public static int find(int a) {
-		if(a == parent[a]) {
-			return a;
-		}else {
-			return parent[a] = find(parent[a]);
+	public static int dfs(int depth) {
+		visited[depth] = true;
+		int cost = arr[depth];
+		
+		for(int i : list.get(depth)) {
+			if(!visited[i]) {
+				visited[i] = true;
+				cost = Math.min(cost, dfs(i));				
+			}
 		}
+		
+		return cost;
 	}
 
 }
+
+/*
+ * 1  2  3  4  5
+ * 10 10 20 20 30
+ * 
+ * 1 3
+ * 2 4
+ * 5 4
+ * 
+ * 각 그룹별 가장 작은 금액
+ * 
+ * 1 3 / 2 4 5
+ * 
+ * 
+ * */
