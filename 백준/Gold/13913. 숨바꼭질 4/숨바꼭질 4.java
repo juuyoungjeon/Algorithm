616c48b5b156672e8011259f7e48557dba2af618
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-11-01 21:09:44 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 344 ms, Memory: 37848 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -3,48 +3,47 @@ import java.io.*;
 
 public class Main {
 	static int N, K;
-	static int[] time;
-	static int[] parent;
+	static int min = Integer.MAX_VALUE;
+	static int[] parent = new int[100001];
+	static int[] time = new int[100001];
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-
-		time = new int[100001];
-		parent = new int[100001];
 		
-		solve();
-		Stack<Integer> s = new Stack<>();
-		s.push(K);
+		bfs(N);
 		
+		Stack<Integer> stack = new Stack<>();
+		stack.push(K);
 		int idx = K;
 		
 		while(idx != N) {
-			s.push(parent[idx]);
+			stack.push(parent[idx]);
 			idx = parent[idx];
 		}
 		
-		sb.append(time[K] - 1 + "\n");
-		
-		while(!s.isEmpty()) {
-			sb.append(s.pop() + " ");
+		sb.append(time[K] - 1 + " \n");
+		while(!stack.isEmpty()) {
+			sb.append(stack.pop() + " ");
 		}
 		
 		System.out.println(sb);
 	}
-	public static void solve() {
+	static int cnt;
+	public static void bfs(int start) {
 		Queue<Integer> q = new LinkedList<>();
-		q.add(N);
-		time[N] = 1;
+		q.add(start);
+		time[start] = 1;
 		
 		while(!q.isEmpty()) {
 			int now = q.poll();
+			
 			if(now == K) return;
+			
 			for(int i = 0; i < 3; i++) {
 				int nx = 0;
 				if(i == 0) {
@@ -54,9 +53,9 @@ public class Main {
 				}else {
 					nx = now * 2;
 				}
-
+				
 				if(nx < 0 || nx > 100000) continue;
-
+				
 				if(time[nx] == 0) {
 					q.add(nx);
 					time[nx] = time[now] + 1;
