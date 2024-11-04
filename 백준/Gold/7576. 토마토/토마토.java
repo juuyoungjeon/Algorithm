55ea644997142aa0ae1285e31b360a5e32a67688
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-11-04 17:33:58 +0900

    [Gold V] Title: 토마토, Time: 632 ms, Memory: 121608 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,73 +2,83 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
-	static int M, N;
-	static int ans;
+	static int N, M;
 	static int[][] map;
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	static Queue<int []> q = new LinkedList<>();
-	
+	static boolean[][] visited;
+	static int[] dx = {1,0,-1,0};
+	static int[] dy = {0,1,0,-1};
+	static class Node{
+		int x;
+		int y;
+		int day;
+		public Node(int x, int y, int day) {
+			this.x = x;
+			this.y = y;
+			this.day = day;
+		}
+	}
+	static int ans;
+	static boolean check;
+	static Queue<Node> q = new LinkedList<>();
 	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		M = Integer.parseInt(st.nextToken());//열
-		N = Integer.parseInt(st.nextToken());//행
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 		
 		map = new int[N][M];
+		visited = new boolean[N][M];
 		
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] == 1) {
-					q.add(new int[] {i, j});
-				}
 			}
 		}
-		System.out.println(bfs());
-	}
-	
-	private static int bfs() {
-		while(!q.isEmpty()) {
-			int[] t = q.poll();
-			int x = t[0];
-			int y = t[1];
-			
-			for(int i = 0; i < 4; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-				if(nx < 0 || nx >= N || ny < 0|| ny >= M) continue;
-				if(map[nx][ny] == 0) {
-					map[nx][ny] = map[x][y] + 1;
-					q.add(new int[] {nx, ny});
-				}
-			}
-		}
-		ans = Integer.MIN_VALUE;
-		if(check()) {
-			return -1;
-		}else {
-			for(int i = 0; i < N; i++) {
-				for(int j = 0; j < M; j++) {
-					ans = Math.max(ans, map[i][j]);
-				}
-			}
-		}
-		return ans-1;
-	}
-
-	private static boolean check() {
-
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < M; j++) {
-				if(map[i][j] == 0)
-					return true;
+				if(map[i][j] == 1) {
+					q.add(new Node(i,j,0));
+					visited[i][j] = true;
+				}
 			}
 		}
-		return false;
+		
+		bfs();
+		
+		if(check()) {
+			System.out.println(ans);
+		}else {
+			System.out.println(-1);
+		}
 	}
-	
+	public static void bfs() {
+		while(!q.isEmpty()) {
+			Node now = q.poll();
+			for(int i = 0; i < 4; i++) {
+				int nx = now.x + dx[i];
+				int ny = now.y + dy[i];
+				
+				if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+				if(visited[nx][ny] || map[nx][ny] == -1) continue;
+				map[nx][ny] = 1;
+				q.add(new Node(nx, ny, now.day + 1));
+				visited[nx][ny] = true;
+			}
+			ans = now.day;
+		}
+	}
+	public static boolean check() {
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				if(map[i][j] == 0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
 }
