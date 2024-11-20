6963e5a2348ca1b8524057324b6053eaa0f0cecc
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2024-11-20 12:52:05 +0900

    [Gold IV] Title: 구슬게임, Time: 208 ms, Memory: 24348 KB -BaekjoonHub

--- 백준/Gold/2600. 구슬게임/구슬게임.java
+++ 백준/Gold/2600. 구슬게임/구슬게임.java
@@ -2,27 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int b1, b2, b3;
-	static int k1, k2;
+	static int b1,b2,b3;
+	static int k1,k2;
 	static int[][] dp;
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		b1 = Integer.parseInt(st.nextToken());
 		b2 = Integer.parseInt(st.nextToken());
 		b3 = Integer.parseInt(st.nextToken());
 		
-		for(int i = 0; i < 5; i++) {
+		for(int i = 0 ; i < 5; i++) {
 			st = new StringTokenizer(br.readLine());
 			k1 = Integer.parseInt(st.nextToken());
 			k2 = Integer.parseInt(st.nextToken());
 			
 			dp = new int[501][501];
-			for(int j = 0; j < 501; j++) {
-				Arrays.fill(dp[j], -1);
+			for(int[] arr : dp) {
+				Arrays.fill(arr, -1);
 			}
 			
 			if(recur(k1, k2)) {
@@ -33,49 +33,44 @@ public class Main {
 		}
 		
 		System.out.println(sb);
-		
-		
 	}
 	
 	public static boolean recur(int cur1, int cur2) {
-		if(cur1 < 0 || cur2 < 0) {
-			return true;
-		}
-		if(cur1 == 0 && cur2 == 0) {
-			return false;
-		}
+		if(cur1 < 0 || cur2 < 0) return true;
 		
-		if(dp[cur1][cur2] != -1) {
-			return dp[cur1][cur2] == 1;
-		}
+		if(cur1 == 0 && cur2 == 0) return false;
 		
-		int cnt = 0;
-		if(!(recur(cur1 - b1, cur2))){
-			cnt++;
+		if(dp[cur1][cur2] != -1) return dp[cur1][cur2] == 1;
+		
+		int ret = 0;
+		
+		if(!(recur(cur1 - b1, cur2))) {
+			ret++;
 		}
-		if(!(recur(cur1 - b2, cur2))){
-			cnt++;
+		if(!(recur(cur1 - b2, cur2))) {
+			ret++;
 		}
-		if(!(recur(cur1 - b3, cur2))){
-			cnt++;
+		if(!(recur(cur1 - b3, cur2))) {
+			ret++;
 		}
 		if(!(recur(cur1, cur2 - b1))) {
-			cnt++;
+			ret++;
 		}
 		if(!(recur(cur1, cur2 - b2))) {
-			cnt++;
+			ret++;
 		}
 		if(!(recur(cur1, cur2 - b3))) {
-			cnt++;
+			ret++;
 		}
 		
-		if(cnt > 0) {
+		if(ret > 0) {
 			dp[cur1][cur2] = 1;
 		}else {
 			dp[cur1][cur2] = 0;
 		}
 		
-		return cnt > 0;
+		return ret > 0;
+		
 	}
 
 }
